f5d946f7d731b0c6970a49040a3ef9d539af644c
r42y9fcmjrawl <username@users.noreply.github.com>
2017-05-05 15:55:15 -0400

    initial commit

---
+++ add_file_permissions.ps1
@@ -0,0 +1,21 @@
+﻿$file = ""
+$user = ""
+
+$acl = Get-Acl -Path $file
+$hasAccess = $false
+
+foreach ($access in $acl.Access) {
+    if ($access.IdentityReference.Value -eq $user) {
+        $hasAccess = $true
+    }
+}
+
+if (!$hasAccess) {
+    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule($user, 'FullControl', 'Allow')
+    $acl.AddAccessRule($rule)
+}
+
+Set-Acl -Path $file -AclObject $acl
+
+$acl = Get-Acl -Path $file
+$acl.Access
---
+++ remove_file_permissions.ps1
@@ -0,0 +1,16 @@
+﻿$file = ""
+$user = ""
+
+$acl = Get-Acl -Path $file
+
+foreach ($access in $acl.Access) {
+    if ($access.IdentityReference.Value -ne $user) {
+        $acl.SetAccessRuleProtection($true, $false)
+        $acl.RemoveAccessRule($access) | Out-Null
+    }
+}
+
+Set-Acl -Path $file -AclObject $acl
+
+$acl = Get-Acl -Path $file
+$acl.Access
